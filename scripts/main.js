6f8cb7f95e953efec101c4bc8b5a06c57fcad5ce
an0209vn <48006180+an0209vn@users.noreply.github.com>
2024-05-15 14:11:55 +0700

    update html

--- scripts/main.js
+++ scripts/main.js
@@ -302,3 +302,21 @@ if(window.screen.width<992){
   $(".srl-form").mCustomScrollbar();
 }
 
+const chooseFile = document.getElementById("choose-file");
+const imgPreview = document.getElementById("img-preview");
+
+chooseFile.addEventListener("change", function () {
+  getImgData();
+});
+
+function getImgData() {
+  const files = chooseFile.files[0];
+  if (files) {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(files);
+    fileReader.addEventListener("load", function () {
+      imgPreview.style.display = "block";
+      imgPreview.innerHTML = '<img src="' + this.result + '" />';
+    });    
+  }
+}
